ca52391bd4d9d28ba02026b2dce3f340f421dbe1
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-06-02 11:02:31 +0300

    implemented the retrieval of available elements for a position

--- SudokuSolver.hs
+++ SudokuSolver.hs
@@ -39,22 +39,38 @@ getLineIndexes index = take 9 [first..]
  -
  - If the user wants to retrieve the indexes on the same column with the index
  - 41 then he will get: [5,14..77]
- -
- - The logic behind the first implementation (filter):
- - I filter out everything in [0..80] that doesn't have the same remainder as the
- - index when doing `mod` 9
- -
- - The logic behind the second implementation:
- - I calculate the start element and then generate the rest of the column by
- - adding 9 to the previous element
  -}
-getColumnIndexes :: Int -> Column
-getColumnIndexes index = take 9 [first,first+9 ..]
+getColIndexes :: Int -> [Int]
+getColIndexes index = take 9 [first,first+9 ..]
     where first = index `mod` 9
 
-getBoxIndexes :: Int -> Box
-getBoxIndexes index = take 3 [start..] -- TODO columns
-    where start = index - (index `mod` 3)
+{- Given an element's index on the board (between 0 and 80) return all indexes
+ - in the same 3x3 box in a 9x9 board
+ -
+ - If the user wants to retrieve the indexes in the same box with the index
+ - 41 then he will get: [30,31,32,39,40,41,48,49,50]
+ -}
+getBoxIndexes :: Int -> [Int]
+getBoxIndexes index = concatMap (\a -> [start+a*9..start+a*9+2]) [0,1,2]
+    where start = 27 * (index `div` 27) + 3 * ((index `mod` 9) `div` 3)
+
+getLineElems :: Int -> [Int] -> [Int]
+getLineElems index board = map (board!!) $ getLineIndexes index
+
+getColElems :: Int -> [Int] -> [Int]
+getColElems index board = map (board!!) $ getColIndexes index
+
+getBoxElems :: Int -> [Int] -> [Int]
+getBoxElems index board = map (board!!) $ getBoxIndexes index
+
+availableElems :: Int -> [Int] -> [Int]
+availableElems index board = [1..9] \\
+    getLineElems index board `union` getColElems index board
+    `union` getBoxElems index board
+
+replaceAtIndex :: Int -> a -> [a] -> [a]
+replaceAtIndex index newElem xs = left ++ [newElem] ++ drop 1 right
+    where (left,right) = splitAt index xs
 
 solve :: SudokuBoard -> SudokuBoard
 solve board = [1,2] -- TODO
